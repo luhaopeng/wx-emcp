f8e89e50f7370777af92fe0b65a1c6d0c5971b17
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-04-23 09:22:26 +0800

    inherit style

--- components/empty/index.jsx
+++ components/empty/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Icon from '../icon'
 import svg from '../../static/img/empty.svg'
+import classNames from 'classnames'
 import './index.less'
 
 class Empty extends React.Component {
     render() {
+        let { style, className } = this.props
         return (
-            <div className={`hl-empty ${this.props.className}`}>
+            <div style={style} className={classNames('hl-empty', className)}>
                 <Icon svg={svg} className='hl-empty-icon' />
                 <p>暂无数据</p>
             </div>
