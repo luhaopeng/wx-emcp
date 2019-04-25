b0707bb939f4903c7191e05c8d131beea1306559
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-04-25 17:06:53 +0800

    temporarily change to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from '../components/app'
-import { basename } from '../util/constants'
 
 ReactDOM.render(
-    <BrowserRouter basename={basename}>
+    <HashRouter>
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 )
