cb6431ef0737b4c161f2922a0703731a43a2dd05
卢浩鹏 <LuHaoPeng@users.noreply.github.com>
2019-04-25 17:05:09 +0800

    simplify build-test config

--- config/webpack.test.config.js
+++ config/webpack.test.config.js
@@ -1,45 +1,14 @@
 const merge = require('webpack-merge')
-const base = require('./webpack.base.config.js')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const prod = require('./webpack.prod.config.js')
 const webpack = require('webpack')
 
-module.exports = merge(base, {
-  mode: 'production',
+module.exports = merge(prod, {
   output: {
     publicPath: '/wxemcp/test/'
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      },
-      {
-        test: /\.less$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          {
-            loader: 'less-loader',
-            options: {
-              javascriptEnabled: true
-            }
-          }
-        ]
-      }
-    ]
-  },
   plugins: [
-    new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('test')
     })
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  }
+  ]
 })
